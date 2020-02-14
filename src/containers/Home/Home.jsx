d5c5d66fa66ef2e901819885006d5a13f263f7c5
Miftah Afina <miftah.afina@gmail.com>
2020-02-14 16:54:55 +0700

    Open whatsapp link when click button

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
+  const [ number, setNumber ] = useState('');
+
+  const handleSend = () => {
+    window.open(`https://api.whatsapp.com/send?phone=${number}`);
+  }
+
+  const handleNumberChange = (event) => {
+    setNumber(event.target.value);
+  }
+
+  useEffect(() => {
+    console.log('number:', number);
+  }, [number]);
+
   return (
     <div>
       <h1>WA Saja</h1>
-      <input type="text" pattern="[0-9]*" inputMode="tel" name="number"/>
+      <form>
+      <input 
+        type="text"
+        pattern="[0-9]*"
+        inputMode="tel"
+        name="number"
+        onChange={handleNumberChange} />
+
       <br />
       <br />
-      <button>Kirim Pesan</button>
+      <button type="submit" onClick={handleSend}>Kirim Pesan</button>
+      </form>
     </div>
   );
 }
