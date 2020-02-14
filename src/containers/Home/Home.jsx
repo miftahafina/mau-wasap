19d001f1f57b97f992b3d343b9d4825f06583036
Miftah Afina <miftah.afina@gmail.com>
2020-02-14 10:33:37 +0700

    Add basic text input with pattern

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>WA Saja</h1>
+      <input type="text" pattern="\d*" />
+      <br />
+      <br />
+      <button>Kirim Pesan</button>
+    </div>
+  );
+}
+
+export default Home;
