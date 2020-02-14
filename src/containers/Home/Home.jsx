baccee7865f1087a63b758bca0b987fecce3293a
Miftah Afina <miftah.afina@gmail.com>
2020-02-14 18:07:32 +0700

    Use material design

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+import {AppBar, Button, Toolbar, Typography, TextField} from '@material-ui/core';
 
 const Home = () => {
   const [ number, setNumber ] = useState('');
@@ -16,21 +17,42 @@ const Home = () => {
   }, [number]);
 
   return (
-    <div>
-      <h1>Mau WA</h1>
-      <form>
-      <input 
-        type="text"
-        pattern="[0-9]*"
-        inputMode="tel"
-        name="number"
-        onChange={handleNumberChange} />
+    <Fragment>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" className="judul">
+            Mau Wasap
+          </Typography>
+        </Toolbar>
+      </AppBar>
 
-      <br />
-      <br />
-      <button type="submit" onClick={handleSend}>Kirim Pesan</button>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      
+
+      <form className="form" noValidate autoComplete="off">
+        <TextField
+          label="Nomor WhatsApp"
+          variant="outlined"
+          pattern="[0-9]"
+          inputMode="tel"
+          name="number"
+          onChange={handleNumberChange} placeholder="62"/>
+
+        <br />
+        <br />
+
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleSend}>
+          Kirim Pesan
+        </Button>
       </form>
-    </div>
+    </Fragment>
   );
 }
 
